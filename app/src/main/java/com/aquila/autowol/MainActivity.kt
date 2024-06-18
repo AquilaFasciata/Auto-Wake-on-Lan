ed3aa2923d194c94f110ca85120d57eaa1b94fe9
Zach Saunders <zachisaunders12@gmail.com>
2024-06-17 23:14:05 -0400

    Added top bar

--- app/src/main/java/com/aquila/autowol/MainActivity.kt
+++ app/src/main/java/com/aquila/autowol/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -23,10 +26,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import com.aquila.autowol.ui.theme.AutoWolTheme
 
 class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
@@ -43,7 +49,18 @@ class MainActivity : ComponentActivity() {
               Icon(Icons.Default.Add, contentDescription = "Ted")
             }
           },
-
+          topBar = {
+            TopAppBar(
+                title = { Text(text = "Auto Wake on Lan") },
+                navigationIcon = {
+                  IconButton(
+                      onClick = { /*TODO*/ },
+                    ) {
+                    Icon(Icons.Default.Menu, contentDescription = "Hamburger Menu")
+                  }
+                },
+              )
+          }
           ) { innerPadding ->
           Greeting(
             name = "Android",
